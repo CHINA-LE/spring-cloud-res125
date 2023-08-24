8b2ff766573556d830a66881ec3bd89c8c748e5e
LE <2905179030@qq.com>
2023-08-24 18:32:26 +0800

    res-init子项目完成

---
+++ res-Init/src/test/java/com/yc/TestDockerInit.java
@@ -0,0 +1,26 @@
+package com.yc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import javax.sql.DataSource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {AppMain.class})
+@ActiveProfiles("dockerinit") // 指定profile类型
+public class TestDockerInit {
+    @Autowired
+//    @Qualifier()
+    private DataSource ds;
+
+    @Test
+    public void testDataSource(){
+        Assert.notNull(ds);
+        System.out.println(ds);
+    }
+}
